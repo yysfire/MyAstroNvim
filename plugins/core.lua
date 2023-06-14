065937c44128169eb45c15497cd7b3fa4eda58fc
yysfire <yysfire+repo@gmail.com>
2023-06-14 10:38:19 +0800

    Enable plugin nvim-dap

--- plugins/core.lua
+++ plugins/core.lua
@@ -22,6 +22,7 @@ return {
   },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
+  { "mfussenegger/nvim-dap", enabled = true },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
