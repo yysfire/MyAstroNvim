fb858593f28fb847dead7b2deb6f72b85ef2d8b7
yysfire <yysfire+repo@gmail.com>
2023-06-14 10:39:36 +0800

    Add the ensure_installed table of treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,22 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "c",
+      "cpp",
+      "dart",
+      "go",
+      "javascript",
+      "json",
+      "jsonc",
+      "lua",
+      "markdown",
+      "python",
+      "rust",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
     })
   end,
 }
