4b9b6a3bf110e27d74edb5df02244705641fedbf
yysfire <yysfire+repo@gmail.com>
2023-06-14 10:32:12 +0800

    Enable wrap and spell for text like documents

--- init.lua
+++ init.lua
@@ -95,5 +95,15 @@ return {
         require("nvim-treesitter.highlight").attach(0, "bash")
       end,
     })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "Enable wrap and spell for text like documents",
+      group = vim.api.nvim_create_augroup("auto_spell", { clear = true }),
+      pattern = { "gitcommit", "markdown", "text", "plaintex" },
+      callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+      end,
+    })
   end,
 }
