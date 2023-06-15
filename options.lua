2bcb0d8d3a1d1bb77ca640836665ec698ac4e571
yysfire <yysfire+repo@gmail.com>
2023-06-15 22:01:24 +0800

    Set vim.opt.shell: pwsh > powershell > cmd > zsh > bash

--- options.lua
+++ options.lua
@@ -1,6 +1,6 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
-return {
-  opt = {
+return function(local_vim)
+  local_vim.opt = {
     -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true,         -- sets vim.opt.number
@@ -9,10 +9,11 @@ return {
     wrap = false,          -- sets vim.opt.wrap
     shiftwidth = 4,        -- sets vim.opt.shiftwidth
     softtabstop = 4,       -- sets vim.opt.softtabstop
-    tabstop = 8,           -- sets vim.opts.tabstop
+    tabstop = 8,           -- sets vim.opt.tabstop
     wrapscan = false,      -- 搜索到文件两端不重新搜索
-  },
-  g = {
+  }
+
+  local_vim.g = {
     mapleader = " ",                 -- sets vim.g.mapleader
     autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true,              -- enable completion at start
@@ -20,14 +21,24 @@ return {
     diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-  },
-}
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
+  }
+
+  if vim.fn.executable("pwsh") or vim.fn.executable("powershell") then
+    local_vim.opt.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
+    local_vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    local_vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    local_vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    local_vim.opt.shellquote = ""
+    local_vim.opt.shellxquote = ""
+  elseif vim.fn.executable("cmd") then
+    local_vim.opt.shell = "cmd"
+  else
+    local_vim.opt.shell = vim.fn.executable("zsh") and "zsh" or "bash"
+  end
+
+  --   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+  --   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+
+  return local_vim
+end
