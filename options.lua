d4bb7ca9f3bbb9311ca354d3a998615c4ec8406b
yysfire <yysfire+repo@gmail.com>
2023-08-15 21:39:03 +0800

    Set vim.g.max_file

--- options.lua
+++ options.lua
@@ -23,6 +23,7 @@ return function(local_vim)
     diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    max_file = { size = 25600000, lines = 100000 },
   }
 
   if vim.fn.executable("pwsh") or vim.fn.executable("powershell") then
